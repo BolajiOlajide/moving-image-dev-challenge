5793121c2a185d32ae82445c9ad2b406a8baeb0f
Bolaji Olajide <cooproton@gmail.com>
2021-01-06 20:42:12 +0100

    add method to process video

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from 'react';
 
-import Header from '../components/Header';
+import type { ProcessedVideo } from '../common/interfaces';
+import { getVideos } from '../services/videos';
 
 const Home: React.FC = () => {
-  // const [videos, setVideos] = useState<ProcessedVideo[]>([]);
+  const [videos, setVideos] = useState<ProcessedVideo[]>([]);
 
-  // useEffect(() => {
-  //   getVideos()
-  //     .then((videos) => {
-  //       setVideos(videos);
-  //     });
-  // }, []);
+  useEffect(() => {
+    getVideos()
+      .then((videos) => {
+        console.log(videos)
+        setVideos(videos);
+      });
+  }, []);
 
   return (
     <main>
--- src/services/videos.ts
+++ src/services/videos.ts
@@ -1,12 +1,33 @@
 import { getCategories } from './categories';
 import { getAuthors } from './authors';
-import { ProcessedVideo } from '../common/interfaces';
+import type { ProcessedVideo, Video } from '../common/interfaces';
 
 export const getVideos = (): Promise<ProcessedVideo[]> => {
   return Promise
     .all([getCategories(),getAuthors()])
     .then(([categories, authors]) => {
-    // TODO: implement
-    return [];
+      const videos: ProcessedVideo[] = [];
+
+      const getCategoryById = (categoryId: number): string => {
+        const foundCategory = categories
+          .find(category => category.id === categoryId);
+        return foundCategory?.name || '';
+      };
+
+      authors.forEach((author) => {
+        const processedVideos: ProcessedVideo[] = author.videos.map((video: Video) => {
+          const processedVideo: ProcessedVideo = {
+            id: video.id,
+            author: author.name,
+            categories: video.catIds.map(getCategoryById),
+            name: video.name
+          };
+
+          return processedVideo;
+        })
+        videos.push(...processedVideos);
+      });
+
+      return videos;
   });
 };
